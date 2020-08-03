bffc27c8a2d4cd3a0d7e92bf647317256973d8c9
Ze Dang <4649tz@gmail.com>
2020-08-03 21:34:50 +0800

    [zedang] test: add test for BowlingGame.getFrameScore()

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,12 +11,40 @@ public class BowlingGameTest {
     @Test
     public void testRoll() {
         BowlingGame game = new BowlingGame();
-        Exception exceptionToLow = assertThrows(IllegalArgumentException.class, () -> game.roll(-1));
-        Exception exceptionToHigh = assertThrows(IllegalArgumentException.class, () -> game.roll(11));
+        Exception exceptionToLow = assertThrows(IllegalArgumentException.class,
+            () -> game.roll(-1));
+        Exception exceptionToHigh = assertThrows(IllegalArgumentException.class,
+            () -> game.roll(11));
         assertAll("roll",
             () -> assertTrue(exceptionToLow.getMessage().contains("Illegal Argument:")),
             () -> assertTrue(exceptionToHigh.getMessage().contains("Illegal Argument:")),
             () -> assertDoesNotThrow(() -> game.roll(5))
+        );
+    }
+
+    @Test
+    public void testGetFrameScore() {
+        int[] records = {10, 9, 1, 4, 3};
+        BowlingGame game = new BowlingGame();
+
+        for (int record : records) {
+            game.roll(record);
+        }
+
+        Exception exceptionFrameOutOfBoundary1 = assertThrows(IllegalArgumentException.class,
+            () -> game.getFrameScore(-1));
+        Exception exceptionFrameOutOfBoundary2 = assertThrows(IllegalArgumentException.class,
+            () -> game.getFrameScore(11));
+
+        assertAll("get_frame_score",
+            () -> assertEquals(20, game.getFrameScore(1)),
+            () -> assertEquals(14, game.getFrameScore(2)),
+            () -> assertEquals(7, game.getFrameScore(3)),
+            () -> assertEquals(0, game.getFrameScore(10)),
+            () -> assertTrue(
+                exceptionFrameOutOfBoundary1.getMessage().contains("Illegal Argument:")),
+            () -> assertTrue(
+                exceptionFrameOutOfBoundary2.getMessage().contains("Illegal Argument:"))
         );
     }
 }

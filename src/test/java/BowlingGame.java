27a6ef7205bf4d53239adbecdb768d7e049931a6
Ze Dang <4649tz@gmail.com>
2020-08-03 13:18:38 +0800

    [zedang] feat: add BowlingGame starter codes.

---
+++ src/test/java/BowlingGame.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+/**
+ * simulates a line of bowling game.
+ *
+ * @author t0iiz
+ */
+public class BowlingGame {
+
+    /** calculates the total score.  */
+    public int getScore() {
+       return 0;
+    }
+
+    /** calculates the points for a specific frame. */
+    public int getFrameScore(int frame) {
+        return 0;
+    }
+
+    /** simulates the action of roll a ball. */
+    public void roll(int num) {
+
+    }
+
+    /** record the status of a frame of the game. */
+    private class Frame {
+        private List<Integer> recordList;
+
+        void addRecord(int record) {
+            return;
+        }
+    }
+}

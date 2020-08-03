280ea178db78a998da8313cc0f90d22ad7c9d44a
Ze Dang <4649tz@gmail.com>
2020-08-03 21:20:18 +0800

    [zedang] test: add test for BowlingGame.roll()

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class BowlingGameTest {
+
+    @Test
+    public void testRoll() {
+        BowlingGame game = new BowlingGame();
+        Exception exceptionToLow = assertThrows(IllegalArgumentException.class, () -> game.roll(-1));
+        Exception exceptionToHigh = assertThrows(IllegalArgumentException.class, () -> game.roll(11));
+        assertAll("roll",
+            () -> assertTrue(exceptionToLow.getMessage().contains("Illegal Argument:")),
+            () -> assertTrue(exceptionToHigh.getMessage().contains("Illegal Argument:")),
+            () -> assertDoesNotThrow(() -> game.roll(5))
+        );
+    }
 }
